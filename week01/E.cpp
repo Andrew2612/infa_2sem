f91308a9f516686c75fd7495dd21da26e8ca61b7
Andrew2612 <112695430+Andrew2612@users.noreply.github.com>
2023-02-02 18:26:03 +0300

    Update E.cpp

--- week01/E.cpp
+++ week01/E.cpp
@@ -1,1 +1,33 @@
+#include <iostream>
+#include <cmath>
 
+int NOD_finder(int n, int m) { /// Находим НОД
+    int k = 1;
+    while (k != 0) {
+            k = n % m;
+            n = m;
+            m = k;
+        }
+    return n;
+}
+
+int main() {
+    int n = 0, m = 0;
+    std::cin >> n >> m;
+
+    if (n == 0 or m == 0)
+        std::cout << "НОК не определён" << std::endl;
+    else {
+        if (n < 0)
+            n = -n;
+        if (m < 0)
+            m = -m;
+
+        int l = 0;
+
+        l = NOD_finder(n, m);
+        std::cout<< n*m/l << std::endl;
+    }
+    
+    return 0;
+}
