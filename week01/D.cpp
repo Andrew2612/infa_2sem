fd0e0e6056e524ba49ca79d976ba08b83d68c7c8
Andrew2612 <112695430+Andrew2612@users.noreply.github.com>
2023-02-02 18:05:30 +0300

    Create D.cpp

---
+++ week01/D.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cmath>
+
+int main() {
+    int n = 0, m = 0, k = 1;
+    std::cin >> n >> m;
+
+    if (n == 0 or m == 0)
+        std::cout << "НОД не определён" << std::endl;
+    else {
+        if (n < 0)
+            n = -n;
+        if (m < 0)
+            m = -m;
+
+        while (k != 0) {
+            k = n % m;
+            n = m;
+            m = k;
+        }
+        std::cout<< n << std::endl;
+    }
+    
+    return 0;
+}
