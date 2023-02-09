03ac7129a7b9a92a8ca77c3d058d1af91e33c67f
Andrew2612 <112695430+Andrew2612@users.noreply.github.com>
2023-02-09 12:06:43 +0300

    Update B.cpp

--- week01/B.cpp
+++ week01/B.cpp
@@ -4,17 +4,21 @@
 int main() {
     int a = 0, b = 0, c = 0;
     std::cin >> a >> b >> c;
-
-    if (b*b - 4*a*c < 0) {
-        std::cout << '\n';
-    }
-
-    else if (b*b - 4*a*c == 0) {
-        std::cout << (b/(2*a)) << std::endl;
+    if (a == 0) {
+        std::cout << -c/b <<std::endl;
     }
     else {
-        std::cout << ((b + sqrt(b*b - 4*a*c))/(2*a)) << ' '
-        << ((b - sqrt(b*b - 4*a*c))/(2*a)) << std::endl;
+        if (b*b - 4*a*c < 0) {
+            std::cout << '\n';
+        }
+
+        else if (b*b - 4*a*c == 0) {
+            std::cout << (b/(2*a)) << std::endl;
+        }
+        else {
+            std::cout << ((b + sqrt(b*b - 4*a*c))/(2*a)) << ' '
+            << ((b - sqrt(b*b - 4*a*c))/(2*a)) << std::endl;
+        }
     }
     return 0;
 }
