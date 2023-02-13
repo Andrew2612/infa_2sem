6b2a71d57ea46cd258ee4277b1fc1bb7f0487517
Andrew3840 <andrew.petuhov03@yandex.ru>
2023-02-13 12:00:21 +0300

    blablabla

--- week02/B.cpp
+++ week02/B.cpp
@@ -1,16 +1,19 @@
 #include <iostream>
 
 int main() {
-    int res = 0;
-
+    int res = -1;
     int x = 0;
+
     std::cin >> x;
+    if (x % 2 == 0) {
+        res++;
+    }
     while (x != 0) {
         std::cin >> x;
         if (x % 2 == 0){
             res++;
         }
     }
-    std::cout << res;
+    std::cout << res << std::endl;
     return 0;
 }
