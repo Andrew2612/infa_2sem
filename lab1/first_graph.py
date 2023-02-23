1254dbc638d1661ca762967102c55fe1e42f8b6c
Andrew2612 <112695430+Andrew2612@users.noreply.github.com>
2023-02-23 16:48:35 +0300

    Update first_graph.py

--- lab1/first_graph.py
+++ lab1/first_graph.py
@@ -4,8 +4,8 @@ import numpy as np
 x = [100, 500, 1000, 2000, 5000, 20000, 50000, 100000, 200000, 500000]
 y = [202, 1048, 1960, 3913, 10065, 39413, 102020, 199750, 407099, 1030386]
 
-plt.xticks(np.arange(1))
-plt.yticks(np.arange(1))
+plt.xticks(np.arange(0, 600000, step = 100000))
+plt.yticks(np.arange(0, 1500000, step = 100000))
 
 plt.scatter(x, y, s=20, c='b')
 plt.plot(x,y)
