127627ecfff5877a99ae32475c311f6a9749eeed
Andrew2612 <112695430+Andrew2612@users.noreply.github.com>
2023-02-23 16:44:55 +0300

    Create first_graph.py

---
+++ lab1/first_graph.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = [100, 500, 1000, 2000, 5000, 20000, 50000, 100000, 200000, 500000]
+y = [202, 1048, 1960, 3913, 10065, 39413, 102020, 199750, 407099, 1030386]
+
+plt.xticks(np.arange(1))
+plt.yticks(np.arange(1))
+
+plt.scatter(x, y, s=20, c='b')
+plt.plot(x,y)
+
+plt.xlabel('Array size')
+plt.ylabel('Time, milliseconds')
+plt.show()
