9db0d3de4c418f2c26f14d6cff94328fa21214aa
Andrew2612 <112695430+Andrew2612@users.noreply.github.com>
2023-02-24 11:14:04 +0300

    Update A.cpp

--- week03/A.cpp
+++ week03/A.cpp
@@ -4,10 +4,6 @@
 #define N 5
 #endif
 
-#ifndef i_love_c_plus_plus
-#define hehehe std::endl
-#endif
-
 void swap(int& lha, int& rha) 
 {
     int tmp = lha;
@@ -16,17 +12,16 @@ void swap(int& lha, int& rha)
 }
 
 int main() {
-    int x = 0;
-    int a[N] = {0}; 
-    std::cin >> x;
-    a[N-1] = x;
-    for (int i = 0; i < N - 1; i++) {
-        std::cin >> x;
-        a[N-2-i] = x;
+    for (int i = 0; i<N; ++i) {
+        cin >> array[i]; 
     }
-    for (int i = 0; i < N; i++) {
-        std::cout << a[i]; 
+
+    for (int i = 0; i<N/2; ++i) {
+        swap(array[i], array[N-1-i]);
     }
-    std::cout << hehehe;
+
+    for (int i = 0; i<N; ++i) {
+        cout << array[i] << ' '; 
+    }
     return 0;
 }
