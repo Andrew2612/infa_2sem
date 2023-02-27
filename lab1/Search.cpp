2cb7c357edd17ce9dbf2f2417735bfb4bf5e36d0
Andrew3840 <andrew.petuhov03@yandex.ru>
2023-02-27 18:48:07 +0300

    blablabla

--- lab1/Search.cpp
+++ lab1/Search.cpp
@@ -3,7 +3,7 @@
 #include <random>
 
 #ifndef N
-#define N 1000
+#define N 500000
 #endif
 
 //uporyadochenni massiv
@@ -26,41 +26,60 @@ void create_array_2(int (&array)[N]) {
 
 int search (int (&arr)[N], int search_for) {
     for (int i = 0; i < N; i++) {
-        if (arr[i] == N+1) {
+        if (arr[i] == search_for) {
             return arr[i];
         }
     }
     return -1;
 }
 
-int bin_search (float left, float right, int search_for) {
-    double epsilon = 1.e-3;
-    double pivot = (left + right) / 2.;
-    double answer = 0.;
-    if (abs(left * left - search_for) < epsilon) {
-        answer = left;
-    } else if (abs(right * right - search_for) < epsilon) {
-        answer = right;
-    } else if (pivot * pivot < search_for) {
-        answer = bin_search(pivot, right, search_for);
-    } else {
-        answer = bin_search(left, pivot, search_for);
+// int bin_search (float left, float right, int search_for) {
+//     double epsilon = 1.e-3;
+//     double pivot = (left + right) / 2.;
+//     double answer = 0.;
+//     if (abs(left * left - search_for) < epsilon) {
+//         answer = left;
+//     } else if (abs(right * right - search_for) < epsilon) {
+//         answer = right;
+//     } else if (pivot * pivot < search_for) {
+//         answer = bin_search(pivot, right, search_for);
+//     } else {
+//         answer = bin_search(left, pivot, search_for);
+//     }
+//     return answer;
+// }
+
+int bin_search (int (&arr)[N], int left, int right, int search_for) {
+    int center = (left + right) / 2.;
+    if (arr[left] == search_for) {
+        return arr[left];
+    } else if (arr[right] == search_for) {
+        return arr[right];
+    } else if (arr[left + 1] == arr[right]){
+        return -1;
+    } else if (arr[right] < search_for) {
+        return -1;
+    }else if (arr[left] > search_for) {
+        return -1;
+    } else if (arr[center] < search_for) {
+        return bin_search(arr, center, right, search_for);
+    } else if (arr[center] > search_for) {
+        return  bin_search(arr, left, center, search_for);
     }
-    return answer;
 }
 
 
 
 int main ( ) {
     int arr[N] = {0};
-    int search_for = N+1;
-    create_array_2(arr);
-    //create_array_1(arr);
+    int search_for = N-10;
+    //create_array_2(arr);
+    create_array_1(arr);
     
     auto begin = std::chrono::steady_clock::now();
     for (int i = 1000; i > 0; i--) {
-        search(arr, search_for);
-        //bin_search(arr[0], arr[N-1],search_for);
+        //search(arr, search_for);
+        bin_search(arr, 0, N-1,search_for);
     }
     auto end = std::chrono::steady_clock::now();
     auto time_span =
--- lab1/graph_0.2.py
+++ lab1/graph_0.2.py
@@ -5,7 +5,7 @@ x = [100, 500, 1000, 2000, 5000, 20000, 50000, 100000, 200000, 500000]
 y = [58, 83, 101, 118, 125, 132, 143, 154, 157, 179]
 
 plt.xticks(np.arange(0, 600000, step = 100000))
-plt.yticks(np.arange(0, 1500000, step = 100000))
+plt.yticks(np.arange(0, 200, step = 25))
 
 plt.scatter(x, y, s=20, c='b')
 plt.plot(x,y)
