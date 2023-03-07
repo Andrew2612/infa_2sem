73181132eeb4f2ab623306680974341590cb6093
Andrew3840 <andrew.petuhov03@yandex.ru>
2023-03-07 22:57:06 +0300

    blablabla

--- lab2/graph4.py
+++ lab2/graph4.py
@@ -2,16 +2,31 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-df = pd.read_csv("hibbardsort.csv")
+df1 = pd.read_csv("combsort.csv")
+df2 = pd.read_csv("hibbardsort.csv")
+df3 = pd.read_csv("fibsort.csv")
 
-N = np.array(df["N"])
-time = np.array(df["number of swaps"])
+N1 = np.array(df1["N"])
+time1 = np.array(df1["number of swaps"])
+
+N2 = np.array(df2["N"])
+time2 = np.array(df2["number of swaps"])
+
+N3 = np.array(df3["N"])
+time3 = np.array(df3["number of swaps"])
+
 fig, ax = plt.subplots(figsize=(6, 7))
 
-ax.scatter(N, time, s=25)
+ax.scatter(N1, time1, s=25)
+ax.scatter(N2, time2, s=25)
+ax.scatter(N3, time3, s=25)
 
-ax.set(xlabel='$N$', ylabel='number of swaps')
+ax.set(xlabel='$N$', ylabel='Number of swaps')
 
-ax.plot(N, time, color='blue')
+line1, = ax.plot(N1, time1, color='blue')
+line2, = ax.plot(N2, time2, color='red')
+line3, = ax.plot(N3, time3, color='black')
+
+ax.legend([line1, line2, line3], ['combSort', 'hibbardSort', 'fibSort'])
 
 plt.show()
