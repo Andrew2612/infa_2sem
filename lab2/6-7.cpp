27de96ad89858d19fd0035a0537d7e7939895ee6
Andrew3840 <andrew.petuhov03@yandex.ru>
2023-03-16 16:23:00 +0300

    blablabla

--- lab2/6-7.cpp
+++ lab2/6-7.cpp
@@ -1,8 +1,7 @@
-#include <iostream>
-#include <random>
 #include <chrono>
 #include <fstream>
-
+#include <iostream>
+#include <random>
 
 #ifndef N
 #define N 100
@@ -15,77 +14,71 @@ int test(int array[], int step) {
                 return -1;
             }
         }
-        step /=2;
+        step /= 2;
     }
     return 0;
 }
 
 void swap(int &i, int &j) {
     unsigned tmp = j;
-    j = i;
-    i = tmp;
+    j            = i;
+    i            = tmp;
 }
 
-void sort (int array[], int &number_of_swaps)
-{
+void sort(int array[], int &number_of_swaps) {
     for (int i = 1; i < N; i++) {
-        for (int j = 1; j < N; j++) {    
-            if (array[j-1] > array[j]) {
-                swap(array[j-1], array[j]);
+        for (int j = 1; j < N; j++) {
+            if (array[j - 1] > array[j]) {
+                swap(array[j - 1], array[j]);
                 number_of_swaps++;
             }
         }
     }
 }
 
-void rasSort (int array[], int& number_of_swaps) 
-{
-    int step = N/2;
+void rasSort(int array[], int &number_of_swaps) {
+    int step = N / 2;
     while (step >= 1) {
-        for (int i = 0; i + step < N; i++)
-		{
-			if (array[i] > array[i + step])
-			{
-				swap(array[i], array[i + step]);
+        for (int i = 0; i + step < N; i++) {
+            if (array[i] > array[i + step]) {
+                swap(array[i], array[i + step]);
                 number_of_swaps++;
-			}
-		}
-		step /= 2;
+            }
+        }
+        step /= 2;
     }
     if (test(array, step) != 0) {
         return;
-    } 
+    }
     sort(array, number_of_swaps);
 }
-
 
 int main() {
     int array[N] = {0};
     std::ofstream out;
     out.open("rassort.csv", std::ios::app);
-    //out << "N,time,number of swaps" << std::endl;
+    // out << "N,time,number of swaps" << std::endl;
 
     int number_of_swaps = 0;
 
-    unsigned seed = 1000-7;
+    unsigned seed = 1000 - 7;
     std::default_random_engine rng(seed);
     std::uniform_int_distribution<int> dstr(0, 100000);
-
 
     for (int j = 0; j < N; j++) {
         array[j] = dstr(rng);
     }
     auto begin = std::chrono::steady_clock::now();
     rasSort(array, number_of_swaps);
-    auto end = std::chrono::steady_clock::now();
+    auto end       = std::chrono::steady_clock::now();
     auto time_span = std::chrono::duration_cast<std::chrono::microseconds>(end - begin);
-    
+
     if (out.is_open()) {
-        out << N << ',' << (float) time_span.count() << "," << number_of_swaps << std::endl;
+        out << N << ',' << (float)time_span.count() << "," << number_of_swaps << std::endl;
     }
     number_of_swaps = 0;
 
     out.close();
-    
+
     return 0;
 }
---
+++ week06/A.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+struct Students
+{
+    std::string name;
+    float result;
+};
+
+int main() {
+    float result = 0;
+    std::string name;
+    float needed_result = 0;
+    // for (int i = 0; i < 10; i++)
+}
